358f72237a9e0e094d39487eca9d70aebdb98a2a
Oka Kurniawan <kurniawano@ieee.org>
2014-08-13 13:12:52 +0800

    Fix setup.py to include example folder.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0.0',
     author='Oka Kurniawan',
     author_email='oka_kurniawan@sutd.edu.sg',
-    packages=['amigobot','libdw','soar2','soar2.io','soar2.outputs'],
+    packages=['amigobot','libdw','soar2','soar2.io','soar2.outputs','examples'],
     url='',
     license='LICENSE.txt',
     description='Package for Digital World.',
